53f691071da2b121979cc703c06a7174e33cbf25
Felipe Lourenzi <felipesiegle@gmail.com>
2024-01-09 23:58:05 -0300

    fix secret

--- missionbrasil/app/api/auth/[...nextauth]/route.js
+++ missionbrasil/app/api/auth/[...nextauth]/route.js
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 
 const authOption = {
+    secret: process.env.SECRET,
     providers: [    
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
